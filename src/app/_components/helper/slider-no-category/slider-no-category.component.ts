e080b381f875eff226765fc9ec801c7cbf9e5933
amitgal45 <amitgal45@gmail.com>
2021-01-16 16:38:50 +0200

    Coded V1.0

--- src/app/_components/helper/slider-no-category/slider-no-category.component.ts
+++ src/app/_components/helper/slider-no-category/slider-no-category.component.ts
@@ -43,15 +43,7 @@ export class SliderNoCategoryComponent implements OnInit {
   getCategory():string{
     return this.category
   }
-  // =new Array<Product>({id:1,name:'Adidas',category:'bzaat',imageUrl:'https://www.freepnglogos.com/uploads/shoes-png/mens-shoes-png-transparent-images-images-11.png',isAvaliable:true,price:60},
-  // {id:2,name:'Nike',category:'bzaat',imageUrl:'https://pngimg.com/uploads/running_shoes/running_shoes_PNG5824.png',isAvaliable:true,price:40},
-  // {id:3,name:'Puma',category:'bzaat',imageUrl:'https://www.freepnglogos.com/uploads/shoes-png/download-nike-shoes-transparent-png-for-designing-projects-16.png',isAvaliable:true,price:30},
-  // {id:4,name:'Diadora',category:'bzaat',imageUrl:'https://www.freepnglogos.com/uploads/shoes-png/shoes-photos-style-guru-fashion-glitz-glamour-27.png',isAvaliable:true,price:70},
-  // {id:5,name:'Diadora',category:'bzaat',imageUrl:'https://www.freepnglogos.com/uploads/shoes-png/download-adidas-shoes-transparent-png-images-icons-18.png',isAvaliable:true,price:70},
-  // {id:6,name:'Puma',category:'bzaat',imageUrl:'https://www.freepnglogos.com/uploads/shoes-png/dance-shoes-png-transparent-dance-shoes-images-36.png',isAvaliable:true,price:30},
-  // {id:7,name:'Nike',category:'bzaat',imageUrl:'https://www.freepnglogos.com/uploads/shoes-png/shoes-woocommerce-for-developers-woocommerce-22.png',isAvaliable:true,price:40},
-  // {id:8,name:'Adidas',category:'bzaat',imageUrl:'https://assets.adidas.com/images/h_2000,f_auto,q_auto:sensitive,fl_lossy/459ff194c35e45ea91b1a8ba00fc4876_9366/NMD_R1_Shoes_Black_B42200_01_standard.jpg',isAvaliable:true,price:60}
-  // ); // decorate the property with @Input()
+
   public count:number;
   public sliderObj:Array<any>;
   public string:string="open";
@@ -72,9 +64,7 @@ export class SliderNoCategoryComponent implements OnInit {
       this.page=0;
     else
       this.page++;
-    // this.sliderObj=this.slide.slice(4,8)
       this.string="closed";
-      // console.log(this.string)
     }, 800)
     // myGreeting
     
@@ -90,16 +80,13 @@ export class SliderNoCategoryComponent implements OnInit {
       console.log(this.page)
     }
     else
-      this.page--;
-      
-    // this.sliderObj=this.slide.slice(0,4)
+      this.page--;      
       this.string="closed";
     },700)
   
   }
   ngOnInit(){
     this.count=0;
-    // this.sliderObj=this.slide.slice(0,4)
   
   }
 
