d192769cb8aaacec693f3fe6abaa4dc030482de3
amitgal45 <amitgal45@gmail.com>
2021-01-19 10:59:14 +0200

    Melese V101

--- src/app/_components/forum/post-comment/post-comment.component.ts
+++ src/app/_components/forum/post-comment/post-comment.component.ts
@@ -2,6 +2,8 @@ import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { PostComment } from 'src/app/_models/post-comment.model';
+import { AuthService } from 'src/app/_services/auth.service';
 import { ForumService } from 'src/app/_services/forum.service';
 
 @Component({
@@ -11,16 +13,22 @@ import { ForumService } from 'src/app/_services/forum.service';
 })
 export class PostCommentComponent implements OnInit {
 
-  constructor(private ForumService:ForumService,private route:ActivatedRoute) { }
-  forumId:number=0;
+  constructor(private ForumService:ForumService,private authService:AuthService,private route:ActivatedRoute) { }
+  postId:number=0;
   onAddComment(form:NgForm){
+    let obj:PostComment = {
+      content:form.value.content,
+      id: Math.round(Math.random()*10),
+      postId:this.postId,
+      userId:this.authService.getUser().id
+    }
     let username="Avi";
-    form.value.postId=this.forumId;
-    this.ForumService.addComment(form.value,username)
+    form.value.postId=this.postId;
+    this.ForumService.addComment(obj,username)
   }
 
   ngOnInit(): void {
-    this.forumId = Number(this.route.snapshot.paramMap.get('id'));
+    this.postId = Number(this.route.snapshot.paramMap.get('id'));
   }
 
 }
