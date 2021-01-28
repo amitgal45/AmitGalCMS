ff8887fb6f18746e11d9e4a764a936595b04b3c3
amitgal45 <amitgal45@gmail.com>
2021-01-28 22:09:48 +0200

    fun

--- src/app/_components/products/slider/slider.component.ts
+++ src/app/_components/products/slider/slider.component.ts
@@ -43,9 +43,9 @@ import { ScrollSlideshowItemComponent } from '../../scroll-slideshow-item/scroll
 })
 export class SliderComponent implements OnInit  {
 
-@Input() slide:Array<any>=new Array<any>({name:'sdsd',model:'sdsd',url:"/assets/slider/slide-img1.jpg"},
-{name:'xxxxxa',model:'sdsd',url:'/assets/slider/slide-img2.jpg'},
-{name:'vvvvvf',model:'sdsd',url:'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a737788e-2be1-4a35-abb7-7d06d4080a62/dbwl6kl-74aa142f-cf69-4606-8766-e07c6f178283.jpg/v1/fill/w_1024,h_683,q_75,strp/elsa_by_kayleighjune_dbwl6kl-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD02ODMiLCJwYXRoIjoiXC9mXC9hNzM3Nzg4ZS0yYmUxLTRhMzUtYWJiNy03ZDA2ZDQwODBhNjJcL2Rid2w2a2wtNzRhYTE0MmYtY2Y2OS00NjA2LTg3NjYtZTA3YzZmMTc4MjgzLmpwZyIsIndpZHRoIjoiPD0xMDI0In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.Ym7zgi9zqZzKD8mVRpdqbdiO1EkayF9fejEHIr0TgJI'},
+@Input() slide:Array<any>=new Array<any>({name:'החליפות המושלמות לגבר',description:'מבצעים עד סוף מרץ על כל החליפות לגבר',model:'מבצעים',url:"/assets/slider/slide-img1.jpg"},
+{name:'עוד שניה כבר קיץ',description:'אבל למי זה משנה עדיין יש קורונה',model:'sdsd',url:'/assets/slider/slide-img2.jpg'},
+{name:'כוכבה הכוכבית מדברת לראשונה',description:'אחרי שפרשה מחיי הדוגמנות כוכבה מפתיעה במקצוע חדש',model:'ראיונות',url:'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a737788e-2be1-4a35-abb7-7d06d4080a62/dbwl6kl-74aa142f-cf69-4606-8766-e07c6f178283.jpg/v1/fill/w_1024,h_683,q_75,strp/elsa_by_kayleighjune_dbwl6kl-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD02ODMiLCJwYXRoIjoiXC9mXC9hNzM3Nzg4ZS0yYmUxLTRhMzUtYWJiNy03ZDA2ZDQwODBhNjJcL2Rid2w2a2wtNzRhYTE0MmYtY2Y2OS00NjA2LTg3NjYtZTA3YzZmMTc4MjgzLmpwZyIsIndpZHRoIjoiPD0xMDI0In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.Ym7zgi9zqZzKD8mVRpdqbdiO1EkayF9fejEHIr0TgJI'},
 ); // decorate the property with @Input()
 public count:number;
 public sliderObj:any;
